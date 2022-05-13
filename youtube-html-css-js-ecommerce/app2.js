d088e30141f33e6a2cab5b912ca0624b4d23754d
Comeron Moussavi <104601506+ComOnDaStix@users.noreply.github.com>
2022-05-13 16:19:35 -0500

    Delete app2.js

--- youtube-html-css-js-ecommerce/app2.js
+++
@@ -1,7 +0,0 @@
-const wrapper = document.querySelector(".sliderWrapper");
-const menuItems = document.querySelectorAll(".menuItem");
-
-menuItems.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      //change the current slide
-      wrapper.style.transform = translateX(`${-100 * index}vw)`;
